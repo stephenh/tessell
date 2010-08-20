372479c630b6912f43b242c40f7d37e2bb9a446d
Stephen Haberman <stephen@exigencecorp.com>
2010-08-20 12:43:40 -0500

    Put in progress sanity checks for StubAnimation.

--- user/src/main/java/org/gwtmpv/widgets/StubAnimation.java
+++ user/src/main/java/org/gwtmpv/widgets/StubAnimation.java
@@ -1,9 +1,21 @@
 package org.gwtmpv.widgets;
 
+/**
+ * Implements {@IsAnimation} for unit tests.
+ *
+ * By default, animations auto-finish. They assume most unit tests are
+ * not interested in their animation functionality and so immediately
+ * run at progress=0 and progress=1.
+ *
+ * For unit tests that <i>do</i> want to test the animation logic,
+ * they should call {@link #doNotAutoFinish()} and then explicitly
+ * {@link #tick(double)} the animation through its range of progress.
+ */
 public class StubAnimation implements IsAnimation {
 
   private final AnimationLogic logic;
   private boolean autoFinish = true;
+  private double currentProgress = -1;
 
   public StubAnimation(AnimationLogic logic) {
     this.logic = logic;
@@ -11,24 +23,33 @@ public class StubAnimation implements IsAnimation {
 
   /** For tests to manually tick the animation through its progress. */
   public void tick(final double progress) {
-    logic.onUpdate(progress);
-    if (progress == 1.0) {
+    if (currentProgress == -1.0 || currentProgress == 1.0) {
+      throw new IllegalStateException("Animation is not currently in progress");
     }
+    logic.onUpdate(progress);
+    currentProgress = progress;
   }
 
+  /**
+   * Mark this instance to not auto-finish and instead wait for a unit
+   * test to explicitly {@link #tick(double)} this to completion.
+   */
   public void doNotAutoFinish() {
     autoFinish = false;
   }
 
   @Override
-  public void run(final int delay) {
-    logic.onUpdate(0);
+  public void run(final int duration) {
+    currentProgress = 0;
     if (autoFinish) {
-      logic.onUpdate(1);
+      tick(0);
+      tick(1);
     }
   }
 
   @Override
   public void cancel() {
+    currentProgress = -1;
   }
+
 }
