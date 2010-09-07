b3ca3847b94b0e920b75af106dba8e536d4f8993
Stephen Haberman <stephen@exigencecorp.com>
2010-09-07 14:27:39 -0600

    Add Model javadocs.

--- user/src/main/java/org/gwtmpv/model/Model.java
+++ user/src/main/java/org/gwtmpv/model/Model.java
@@ -1,7 +1,9 @@
 package org.gwtmpv.model;
 
+/** Model is a wrapper around a DTO {@code D} that has a collection of properties. */
 public interface Model<D> {
 
+  /** Merges the new values from {@code dto} into the model's properties. */
   void merge(D dto);
 
 }
