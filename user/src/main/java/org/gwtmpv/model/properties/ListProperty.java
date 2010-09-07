0db791084ad30bf45b238041a57536ec72fb4a5f
Stephen Haberman <stephen@exigencecorp.com>
2010-09-07 12:44:06 -0600

    Remove unneeded ListProperty.addExisting.

--- user/src/main/java/org/gwtmpv/model/properties/ListProperty.java
+++ user/src/main/java/org/gwtmpv/model/properties/ListProperty.java
@@ -5,8 +5,8 @@ import static org.gwtmpv.model.properties.NewProperty.integerProperty;
 import java.util.ArrayList;
 
 import org.gwtmpv.model.events.ValueAddedEvent;
-import org.gwtmpv.model.events.ValueRemovedEvent;
 import org.gwtmpv.model.events.ValueAddedEvent.ValueAddedHandler;
+import org.gwtmpv.model.events.ValueRemovedEvent;
 import org.gwtmpv.model.events.ValueRemovedEvent.ValueRemovedHandler;
 import org.gwtmpv.model.values.DerivedValue;
 import org.gwtmpv.model.values.Value;
@@ -19,17 +19,12 @@ public class ListProperty<E> extends AbstractProperty<ArrayList<E>, ListProperty
     super(value);
   }
 
-  // not sure why addExisting exists
-  public void addExisting(final E item) {
+  public void add(final E item) {
     get().add(item);
     setTouched(true);
     fireEvent(new ValueAddedEvent<E>(this, item));
     lastValue = null; // force changed
     reassess();
-  }
-
-  public void add(final E item) {
-    addExisting(item);
   }
 
   public void remove(final E item) {
