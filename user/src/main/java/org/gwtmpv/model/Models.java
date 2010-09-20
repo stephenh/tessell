ae2ef52e6ed7fada53bda3c42b76bffd077c2279
Stephen Haberman <stephen@exigencecorp.com>
2010-09-20 17:50:51 -0600

    Add Models with helper methods.

---
+++ user/src/main/java/org/gwtmpv/model/Models.java
@@ -0,0 +1,16 @@
+package org.gwtmpv.model;
+
+import java.util.ArrayList;
+
+/** Utility methods for models. */
+public class Models {
+
+  public static <M extends Model<D>, D extends Dto<M>> ArrayList<M> toModels(ArrayList<D> dtos) {
+    ArrayList<M> models = new ArrayList<M>();
+    for (D dto : dtos) {
+      models.add(dto.toModel());
+    }
+    return models;
+  }
+
+}
