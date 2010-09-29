e0a0a36158767e1f4864674e7071290616bec15b
Stephen Haberman <stephen@exigencecorp.com>
2010-09-29 12:52:57 -0600

    Add EntityDto.

---
+++ user/src/main/java/org/gwtmpv/model/EntityDto.java
@@ -0,0 +1,15 @@
+package org.gwtmpv.model;
+
+/**
+ * A dto for an entity.
+ *
+ * Enables {@code model <-> dto} logic to be abstracted.
+ *
+ * @param K the type of the entity key (Integer/Long)
+ * @param M the type of the model
+ */
+public interface EntityDto<K, M> extends Dto<M> {
+
+  K id();
+
+}
