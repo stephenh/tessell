03b4986ceaa8ca1296b980eb379a04dc96463891
Stephen Haberman <stephen@exigencecorp.com>
2010-10-29 22:24:37 -0600

    Have BoundColumn be it's own FieldUpdater, needs tests.

--- user/src/main/java/org/gwtmpv/widgets/cellview/BoundColumn.java
+++ user/src/main/java/org/gwtmpv/widgets/cellview/BoundColumn.java
@@ -7,11 +7,18 @@ import org.gwtmpv.util.UserStringable;
 
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.view.client.ProvidesKey;
 
-public class BoundColumn<T, C> extends Column<T, C> {
+/**
+ * Provides the "value getting" part of table columns by wrapping a bindgen-generated binding.
+ *
+ * @param <T> the row type (root binding type in bindgen)
+ * @param <C> the value type
+ */
+public class BoundColumn<T, C> extends Column<T, C> implements FieldUpdater<T, C> {
 
   private final BindingRoot<T, C> binding;
   private String styleName;
@@ -80,11 +87,17 @@ public class BoundColumn<T, C> extends Column<T, C> {
   public BoundColumn(final BindingRoot<T, C> binding, final Cell<C> cell) {
     super(cell);
     this.binding = binding;
+    setFieldUpdater(this);
   }
 
   @Override
   public C getValue(final T row) {
     return binding.getWithRoot(row);
+  }
+
+  @Override
+  public void update(int index, T object, C value) {
+    binding.setWithRoot(object, value);
   }
 
   public BoundColumn<T, C> styleName(final String styleName) {
