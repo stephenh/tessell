223ea45545f697de0f245e54d5e719fb7b6cb758
Stephen Haberman <stephen@exigencecorp.com>
2010-11-02 13:17:45 -0600

    Set the StubCellValue for cells in headers.

--- user/src/main/java/org/gwtmpv/widgets/cellview/StubHeader.java
+++ user/src/main/java/org/gwtmpv/widgets/cellview/StubHeader.java
@@ -1,5 +1,7 @@
 package org.gwtmpv.widgets.cellview;
 
+import org.gwtmpv.widgets.cellview.StubCell.StubCellValue;
+
 import com.google.gwt.cell.client.Cell;
 import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.user.cellview.client.Header;
@@ -9,9 +11,20 @@ public class StubHeader<C> implements IsHeader<C> {
   private final HeaderValue<C> value;
   private final Cell<C> cell;
 
-  public StubHeader(HeaderValue<C> value, Cell<C> cell) {
+  public StubHeader(final HeaderValue<C> value, final Cell<C> cell) {
     this.value = value;
     this.cell = cell;
+    ((StubCell<C>) cell).setStubCellValue(new StubCellValue<C>() {
+      @Override
+      public C getValue(int displayedIndex) {
+        return value.get();
+      }
+
+      @Override
+      public void setValue(int displayedIndex, C newValue) {
+        value.set(newValue);
+      }
+    });
   }
 
   @Override
