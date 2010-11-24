fd8992a32794872433f7a89582f1a959a17320c4
Stephen Haberman <stephen@exigencecorp.com>
2010-11-24 15:55:49 -0600

    Add GWT 2.1 HasEnabled characteristic interface to IsFocusWidget.

--- user/src/main/java/org/gwtmpv/widgets/IsFocusWidget.java
+++ user/src/main/java/org/gwtmpv/widgets/IsFocusWidget.java
@@ -4,9 +4,9 @@ import com.google.gwt.event.dom.client.HasAllFocusHandlers;
 import com.google.gwt.event.dom.client.HasAllKeyHandlers;
 import com.google.gwt.event.dom.client.HasAllMouseHandlers;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.HasEnabled;
 
-public interface IsFocusWidget extends IsWidget, HasClickHandlers, HasAllFocusHandlers, HasAllKeyHandlers,
-    HasAllMouseHandlers {
+public interface IsFocusWidget extends IsWidget, HasClickHandlers, HasAllFocusHandlers, HasAllKeyHandlers, HasAllMouseHandlers, HasEnabled {
 
   int getTabIndex();
 
