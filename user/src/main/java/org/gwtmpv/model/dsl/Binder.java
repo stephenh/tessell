90a902b085334a723611fecb63cbd43c62fe04ee
Stephen Haberman <stephen@exigencecorp.com>
2010-12-09 22:13:21 -0600

    Add a few more flavors of Binder.enhance. Not perfect yet.

--- user/src/main/java/org/gwtmpv/model/dsl/Binder.java
+++ user/src/main/java/org/gwtmpv/model/dsl/Binder.java
@@ -57,21 +57,70 @@ public class Binder {
 
   /** Enhances each {@code source} to fire change events on key up and blur. */
   public <P, S extends HasKeyUpHandlers & HasBlurHandlers & HasValue<P>> Binder enhance(S... sources) {
+    fireChangeOnBlur(sources);
+    fireChangeOnKeyUp(sources);
+    return this;
+  }
+
+  /** Forces a {@link ValueChangeEvent} (and hence touching) of each {@code source} on blur. */
+  public <P, S extends HasBlurHandlers & HasValue<P>> Binder fireChangeOnBlur(S... sources) {
     for (final S source : sources) {
-      registerHandler(source.addKeyUpHandler(new KeyUpHandler() {
-        public void onKeyUp(final KeyUpEvent event) {
-          // if they have just tabbed into the field, we'll get a key up for tab, but ignore it.
-          if (event.getNativeKeyCode() == KEY_TAB) {
-            return;
-          }
-          ValueChangeEvent.fire(source, source.getValue());
-        }
-      }));
       registerHandler(source.addBlurHandler(new BlurHandler() {
         public void onBlur(final BlurEvent event) {
           ValueChangeEvent.fire(source, source.getValue());
         }
       }));
+    }
+    return this;
+  }
+
+  /** Forces a {@link ValueChangeEvent} (and hence touching) of each {@code source} on key up. */
+  public <P, S extends HasKeyUpHandlers & HasValue<P>> Binder fireChangeOnKeyUp(S... sources) {
+    for (final S source : sources) {
+      registerHandler(source.addKeyUpHandler(new KeyUpHandler() {
+        public void onKeyUp(final KeyUpEvent event) {
+          if (event.getNativeKeyCode() == KEY_TAB) {
+            return; // ignore tabbing into the field
+          }
+          ValueChangeEvent.fire(source, source.getValue());
+        }
+      }));
+    }
+    return this;
+  }
+
+  /*
+   * Properties might need two kinds of values--current and changing. E.g. property.changing()
+   * is the same value, except that it includes results from key up events. Then derivative
+   * properties can be made off of either the current or changing property
+   * 
+   * Think of the login email box case:
+   *
+   * 1. User starts typing "foo@" -- do not do validation.
+   * 1a. The 'remaining' box is counting down from 100, 99, 98... chars left
+   * 2. User finishes typing "foo@bar.com!" -- stays in the box -- changed after X seconds (fancy, probably not)
+   * 3. User tabs out of the email box, blur/change fires, do validation
+   * 4. User fixes email by removing "!", redo validation as soon as key up is fired
+   *
+   * Or is 'remaining' just a view artifact, that is driven directly by key up,
+   * textBox.getValue and not by the backing model property?
+   * 
+   * Maybe that makes more sense--the model is only updated when the user
+   * is no longer in input mode. While in input mode, the view can display
+   * various view-specific content, like length of input remaining. But
+   * there is no reason for the in-progress state to leak back into the model.
+   * 
+   * ...but what about in error-mode correction? After a field/model have
+   * been touched, and determined invalid, don't we want to let the user
+   * know they've remedied the error ASAP, e.g. after key up? After a pause?
+   */
+
+  /** Forces a {@link ValueChangeEvent} of each {@code source} on key up, after it's been touched. */
+  public <P, S extends HasKeyUpHandlers & HasBlurHandlers & HasValue<P>> Binder fireChangeOnBlurThenKeyUp(S... sources) {
+    for (final S source : sources) {
+      BlurThenKeyUp<P> h = new BlurThenKeyUp<P>(source);
+      registerHandler(source.addKeyUpHandler(h));
+      registerHandler(source.addBlurHandler(h));
     }
     return this;
   }
@@ -98,6 +147,33 @@ public class Binder {
       }
       super.to(source);
       return this;
+    }
+  }
+
+  /** Fires {@link ValueChangeEvent} on blur (always) and key up (after a blur has been fired). */
+  private static class BlurThenKeyUp<P> implements KeyUpHandler, BlurHandler {
+    private final HasValue<P> source;
+    private boolean hasBlurred = false;
+
+    private BlurThenKeyUp(HasValue<P> source) {
+      this.source = source;
+    }
+
+    @Override
+    public void onBlur(BlurEvent event) {
+      hasBlurred = true;
+      ValueChangeEvent.fire(source, source.getValue());
+    }
+
+    @Override
+    public void onKeyUp(KeyUpEvent event) {
+      if (!hasBlurred) {
+        return;
+      }
+      if (event.getNativeKeyCode() == KEY_TAB) {
+        return; // ignore tabbing into the field
+      }
+      ValueChangeEvent.fire(source, source.getValue());
     }
   }
 
