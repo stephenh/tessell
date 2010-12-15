1fde53913572161f733cb0413212a2cee4f5a8c0
Stephen Haberman <stephen@exigencecorp.com>
2010-12-15 17:40:23 -0600

    Fix bug in StubHTMLPanel.add.

--- user/src/main/java/org/gwtmpv/widgets/StubHTMLPanel.java
+++ user/src/main/java/org/gwtmpv/widgets/StubHTMLPanel.java
@@ -1,15 +1,12 @@
 package org.gwtmpv.widgets;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
 
   private final String html;
   private final Map<String, IsWidget> replaced = new HashMap<String, IsWidget>();
-  private final List<IsWidget> placed = new ArrayList<IsWidget>();
 
   public StubHTMLPanel() {
     html = null;
@@ -21,7 +18,7 @@ public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
 
   @Override
   public void add(IsWidget widget, IsElement elem) {
-    placed.add(widget);
+    super.add(widget);
   }
 
   public String getHtml() {
@@ -30,17 +27,13 @@ public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
 
   @Override
   public void addAndReplaceElement(IsWidget widget, IsElement elem) {
-    add(widget);
+    super.add(widget);
   }
 
   @Override
   public void addAndReplaceElement(IsWidget widget, String id) {
-    add(widget);
+    super.add(widget);
     replaced.put(id, widget);
-  }
-
-  public IsWidget getPlaced(int index) {
-    return placed.get(index);
   }
 
   public IsWidget getReplaced(String id) {
