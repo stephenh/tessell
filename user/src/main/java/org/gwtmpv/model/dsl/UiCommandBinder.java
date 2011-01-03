c5c8c9323b67297c10895403ca56c7dcc582d7f5
Stephen Haberman <stephen@exigencecorp.com>
2011-01-02 23:35:13 -0600

    Add binder.bind(command).toEnterKey(HasKeyUpHandlers...).

--- user/src/main/java/org/gwtmpv/model/dsl/UiCommandBinder.java
+++ user/src/main/java/org/gwtmpv/model/dsl/UiCommandBinder.java
@@ -1,5 +1,7 @@
 package org.gwtmpv.model.dsl;
 
+import static com.google.gwt.event.dom.client.KeyCodes.KEY_ENTER;
+
 import org.gwtmpv.model.commands.UiCommand;
 import org.gwtmpv.model.properties.Property;
 import org.gwtmpv.widgets.IsTextList;
@@ -7,6 +9,9 @@ import org.gwtmpv.widgets.IsTextList;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasKeyUpHandlers;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 
 /** Binds various things to a command. */
 public class UiCommandBinder {
@@ -22,6 +27,20 @@ public class UiCommandBinder {
   /** Binds clicks from {@code clickable} to our command, and our errors to {@code errors}. */
   public UiCommandBinder to(final HasClickHandlers clickable, final IsTextList errors) {
     return to(clickable).errorsTo(errors);
+  }
+
+  /** Binds "enter" from key up handlers to our command. */
+  public UiCommandBinder toEnterKey(final HasKeyUpHandlers... keyUps) {
+    for (HasKeyUpHandlers keyUp : keyUps) {
+      binder.registerHandler(keyUp.addKeyUpHandler(new KeyUpHandler() {
+        public void onKeyUp(KeyUpEvent event) {
+          if (event.getNativeKeyCode() == KEY_ENTER) {
+            command.execute();
+          }
+        }
+      }));
+    }
+    return this;
   }
 
   /** Has our command execute only if {@code onlyIf} is true. */
