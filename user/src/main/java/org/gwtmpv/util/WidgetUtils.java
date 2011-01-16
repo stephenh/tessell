2122053d8b705ea99fbab8f1f187fd16ad70b34b
Stephen Haberman <stephen@exigencecorp.com>
2011-01-15 23:05:39 -0600

    Add WidgetUtils.focus.

--- user/src/main/java/org/gwtmpv/util/WidgetUtils.java
+++ user/src/main/java/org/gwtmpv/util/WidgetUtils.java
@@ -4,8 +4,11 @@ import static org.gwtmpv.widgets.Widgets.newAnimation;
 
 import org.gwtmpv.widgets.AnimationLogic;
 import org.gwtmpv.widgets.HasCss;
+import org.gwtmpv.widgets.IsFocusWidget;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.event.logical.shared.AttachEvent;
 
 public class WidgetUtils {
 
@@ -57,6 +60,21 @@ public class WidgetUtils {
   public static void show(HasCss... csses) {
     for (HasCss css : csses) {
       css.getStyle().clearDisplay();
+    }
+  }
+
+  /** Sets focus on {@code w}, potentially waiting until it's {@link AttachEvent}. */
+  public static void focus(final IsFocusWidget w) {
+    if (w.isAttached() || !GWT.isClient()) {
+      w.setFocus(true);
+    } else {
+      w.addAttachHandler(new AttachEvent.Handler() {
+        public void onAttachOrDetach(AttachEvent event) {
+          if (event.isAttached()) {
+            w.setFocus(true);
+          }
+        }
+      });
     }
   }
 
