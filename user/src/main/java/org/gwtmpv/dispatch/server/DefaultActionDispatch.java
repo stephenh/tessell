6bb67cb712299ba60a078b8bdc73c2cb91afdd4a
Stephen Haberman <stephen@exigencecorp.com>
2011-01-23 10:36:15 -0600

    Remove ActionException wrapping from DefaultActionDispatch, leave it up to the servlet.

--- user/src/main/java/org/gwtmpv/dispatch/server/DefaultActionDispatch.java
+++ user/src/main/java/org/gwtmpv/dispatch/server/DefaultActionDispatch.java
@@ -3,7 +3,6 @@ package org.gwtmpv.dispatch.server;
 import org.gwtmpv.dispatch.server.handlers.ActionHandler;
 import org.gwtmpv.dispatch.server.handlers.ActionHandlerRegistry;
 import org.gwtmpv.dispatch.shared.Action;
-import org.gwtmpv.dispatch.shared.ActionException;
 import org.gwtmpv.dispatch.shared.Result;
 
 /** Stock server-side implementation of {@link ActionDispatch}. */
@@ -11,18 +10,10 @@ public class DefaultActionDispatch implements ActionDispatch {
 
   protected final ActionHandlerRegistry handlers = new ActionHandlerRegistry();
 
-  /** Executes {@code action} and ensures only {@link ActionException}s are thrown. */
+  /** Executes {@code action}. */
   @Override
-  public <A extends Action<R>, R extends Result> R execute(final A action, final ExecutionContext context) throws ActionException {
-    try {
-      return findHandler(action).execute(action, context);
-    } catch (ActionException ae) {
-      log(action, context, ae);
-      throw ae; // allow ActionException subclasses to go back
-    } catch (Exception e) {
-      log(action, context, e);
-      throw wrapInActionException(e);
-    }
+  public <A extends Action<R>, R extends Result> R execute(final A action, final ExecutionContext context) {
+    return findHandler(action).execute(action, context);
   }
 
   /** Adds {@code handler}. */
@@ -37,15 +28,6 @@ public class DefaultActionDispatch implements ActionDispatch {
       throw new IllegalStateException("No handler for " + action);
     }
     return handler;
-  }
-
-  /** Subclasses should override to provide logging. */
-  protected void log(final Action<?> action, final ExecutionContext context, Exception e) {
-  }
-
-  /** Subclasses can override to change the ActionException construction. */
-  protected ActionException wrapInActionException(Exception e) {
-    return new ActionException("A server error occurred"); // don't leak the raw exception message
   }
 
   protected ActionHandlerRegistry getHandlerRegistry() {
--- user/src/main/java/org/gwtmpv/dispatch/server/servlet/AbstractDispatchServiceServlet.java
+++ user/src/main/java/org/gwtmpv/dispatch/server/servlet/AbstractDispatchServiceServlet.java
@@ -2,6 +2,8 @@ package org.gwtmpv.dispatch.server.servlet;
 
 import static org.gwtmpv.util.ObjectUtils.eq;
 
+import javax.servlet.GenericServlet;
+
 import org.gwtmpv.dispatch.server.ActionDispatch;
 import org.gwtmpv.dispatch.server.ExecutionContext;
 import org.gwtmpv.dispatch.server.SessionIdValidator;
@@ -32,10 +34,22 @@ public abstract class AbstractDispatchServiceServlet extends RemoteServiceServle
       }
       return d.execute(action, context);
     } catch (final ActionException ae) {
+      // assume the user has already logged the ActionException appropriately
       throw ae;
     } catch (final Exception e) {
-      throw new ActionException("A server error occured."); // don't leak the raw exception message
+      log(e);
+      throw wrapInActionException(e);
     }
+  }
+
+  /** Allows subclasses to override exception logging. By default uses {@link GenericServlet#log}. */
+  protected void log(Exception e) {
+    log(e.getMessage(), e);
+  }
+
+  /** Allows subclasses to create their own "runtime exception" subclass of {@link ActionException}. */
+  protected ActionException wrapInActionException(Exception e) {
+    return new ActionException("A server error occured."); // don't leak the raw exception message
   }
 
   protected abstract SessionIdValidator getSessionValidator();
