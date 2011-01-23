b628858830d116c11ee60d8b927be093dc546d70
Stephen Haberman <stephen@exigencecorp.com>
2011-01-23 10:42:35 -0600

    Reword as ActionException isn't checked.

--- user/src/main/java/org/gwtmpv/dispatch/shared/ActionException.java
+++ user/src/main/java/org/gwtmpv/dispatch/shared/ActionException.java
@@ -1,13 +1,12 @@
 package org.gwtmpv.dispatch.shared;
 
-
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 /**
- * Checked exception to return from {@link DispatchService}.
+ * A specific exception to return from {@link DispatchService}.
  * 
  * GWT doesn't like random server-side RuntimeExceptions making their way down to the client (results in a serialization
- * exception), so we wrap all Exceptions in an ActionException.
+ * exception), so we convert all Exceptions to an ActionException (or an ActionException subclass).
  * 
  * Given without the checked exception in an RPC interface, you won't be able to use exceptions anyway, your dispatch
  * results should use return codes for anything but a truly "reload-the-application-please" exception.
