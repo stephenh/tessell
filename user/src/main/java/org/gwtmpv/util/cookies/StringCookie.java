23022ad1f456d1f6327a10257c19540c1ea98e34
Stephen Haberman <stephen@exigencecorp.com>
2011-02-14 11:42:32 -0600

    Add another StringCookie cstr.

--- user/src/main/java/org/gwtmpv/util/cookies/StringCookie.java
+++ user/src/main/java/org/gwtmpv/util/cookies/StringCookie.java
@@ -1,11 +1,17 @@
 package org.gwtmpv.util.cookies;
 
+import java.util.Date;
+
 import org.gwtmpv.util.cookies.facade.IsCookies;
 
 public class StringCookie extends AbstractCookie<String> {
 
   public StringCookie(final IsCookies cookies, final String name) {
     super(cookies, name);
+  }
+
+  public StringCookie(final IsCookies cookies, String name, String domain, Date expires, boolean secure) {
+    super(cookies, name, domain, expires, secure);
   }
 
   @Override
