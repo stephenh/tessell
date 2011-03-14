c30c4fb36d66d3130a1fde36d87bf9eca364d777
Stephen Haberman <stephen@exigencecorp.com>
2011-03-13 22:35:00 -0500

    Add IsCellList.setValueUpdater.

--- user/src/main/java/org/gwtmpv/widgets/IsCellList.java
+++ user/src/main/java/org/gwtmpv/widgets/IsCellList.java
@@ -1,5 +1,6 @@
 package org.gwtmpv.widgets;
 
+import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.safehtml.shared.SafeHtml;
 
 public interface IsCellList<T> extends IsWidget, IsAbstractHasData<T> {
@@ -7,5 +8,7 @@ public interface IsCellList<T> extends IsWidget, IsAbstractHasData<T> {
   SafeHtml getEmptyListMessage();
 
   void setEmptyListMessage(SafeHtml safeHtml);
+
+  void setValueUpdater(ValueUpdater<T> valueUpdater);
 
 }
--- user/src/main/java/org/gwtmpv/widgets/StubCellList.java
+++ user/src/main/java/org/gwtmpv/widgets/StubCellList.java
@@ -1,6 +1,10 @@
 package org.gwtmpv.widgets;
 
+import org.gwtmpv.widgets.cellview.StubCell;
+import org.gwtmpv.widgets.cellview.StubCell.StubCellValue;
+
 import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.safehtml.shared.SafeHtml;
 
 public class StubCellList<T> extends AbstractStubHasDataWidget<T> implements IsCellList<T> {
@@ -8,9 +12,21 @@ public class StubCellList<T> extends AbstractStubHasDataWidget<T> implements IsC
   @SuppressWarnings("unused")
   private final Cell<T> cell;
   private SafeHtml emptyListMessage;
+  private ValueUpdater<T> valueUpdater;
 
   public StubCellList(Cell<T> cell) {
     this.cell = cell;
+    ((StubCell<T>) cell).setStubCellValue(new StubCellValue<T>() {
+      public T getValue(int displayedIndex) {
+        return getVisibleItem(displayedIndex);
+      }
+
+      @Override
+      public void setValue(int displayedIndex, T value) {
+        // odd that displayedIndex is ignored
+        valueUpdater.update(value);
+      }
+    });
   }
 
   @Override
@@ -21,6 +37,11 @@ public class StubCellList<T> extends AbstractStubHasDataWidget<T> implements IsC
   @Override
   public void setEmptyListMessage(SafeHtml safeHtml) {
     this.emptyListMessage = safeHtml;
+  }
+
+  @Override
+  public void setValueUpdater(ValueUpdater<T> valueUpdater) {
+    this.valueUpdater = valueUpdater;
   }
 
 }
