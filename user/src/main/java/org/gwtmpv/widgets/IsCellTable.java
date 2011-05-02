1eafacbcb6d2c23be90f7dffc0bed5bd1a1facc7
Stephen Haberman <stephen@exigencecorp.com>
2011-05-02 14:54:48 -0500

    Add lots of IsCellTable methods.

--- user/src/main/java/org/gwtmpv/widgets/IsCellTable.java
+++ user/src/main/java/org/gwtmpv/widgets/IsCellTable.java
@@ -3,13 +3,36 @@ package org.gwtmpv.widgets;
 import org.gwtmpv.widgets.cellview.IsColumn;
 import org.gwtmpv.widgets.cellview.IsHeader;
 
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
+import com.google.gwt.user.cellview.client.ColumnSortList;
+
 public interface IsCellTable<T> extends IsWidget, IsAbstractHasData<T> {
 
   void addColumn(IsColumn<T, ?> col);
 
+  void addColumn(IsColumn<T, ?> col, String headerString);
+
+  void addColumn(IsColumn<T, ?> col, SafeHtml headerHtml);
+
   void addColumn(IsColumn<T, ?> col, IsHeader<?> header);
 
   void addColumn(IsColumn<T, ?> col, IsHeader<?> header, IsHeader<?> footer);
+
+  void addColumn(IsColumn<T, ?> col, String headerString, String footerString);
+
+  void addColumn(IsColumn<T, ?> col, SafeHtml headerHtml, SafeHtml footerHtml);
+
+  HandlerRegistration addColumnSortHandler(ColumnSortEvent.Handler handler);
+
+  int getColumnCount();
+
+  IsColumn<T, ?> getIsColumn(int col);
+
+  int getColumnIndex(IsColumn<T, ?> column);
+
+  ColumnSortList getColumnSortList();
 
   void removeColumn(IsColumn<T, ?> col);
 
@@ -18,5 +41,7 @@ public interface IsCellTable<T> extends IsWidget, IsAbstractHasData<T> {
   void redrawHeaders();
 
   void redrawFooters();
+
+  void setWidth(String width, boolean isFixedLayout);
 
 }
--- user/src/main/java/org/gwtmpv/widgets/StubCellTable.java
+++ user/src/main/java/org/gwtmpv/widgets/StubCellTable.java
@@ -1,5 +1,8 @@
 package org.gwtmpv.widgets;
 
+import static org.gwtmpv.widgets.cellview.Cells.newSafeHtmlHeader;
+import static org.gwtmpv.widgets.cellview.Cells.newTextHeader;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,11 +11,17 @@ import org.gwtmpv.widgets.cellview.IsColumn;
 import org.gwtmpv.widgets.cellview.IsHeader;
 import org.gwtmpv.widgets.cellview.StubColumn;
 
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
+import com.google.gwt.user.cellview.client.ColumnSortList;
+
 public class StubCellTable<T> extends StubAbstractHasDataWidget<T> implements IsCellTable<T> {
 
   private final List<IsColumn<T, ?>> columns = new ArrayList<IsColumn<T, ?>>();
   private final List<IsHeader<?>> headers = new ArrayList<IsHeader<?>>();
   private final List<IsHeader<?>> footers = new ArrayList<IsHeader<?>>();
+  private final ColumnSortList sortList = new ColumnSortList();
 
   public StubCellTable() {
   }
@@ -49,7 +58,7 @@ public class StubCellTable<T> extends StubAbstractHasDataWidget<T> implements Is
 
   @Override
   public void addColumn(final IsColumn<T, ?> col) {
-    addColumn(col, null, null);
+    addColumn(col, (IsHeader<?>) null, (IsHeader<?>) null);
   }
 
   @Override
@@ -85,6 +94,54 @@ public class StubCellTable<T> extends StubAbstractHasDataWidget<T> implements Is
     columns.remove(index);
     headers.remove(index);
     footers.remove(index);
+  }
+
+  @Override
+  public int getColumnCount() {
+    return columns.size();
+  }
+
+  @Override
+  public IsColumn<T, ?> getIsColumn(int col) {
+    return columns.get(col);
+  }
+
+  @Override
+  public int getColumnIndex(IsColumn<T, ?> column) {
+    return columns.indexOf(column);
+  }
+
+  @Override
+  public ColumnSortList getColumnSortList() {
+    return sortList;
+  }
+
+  @Override
+  public HandlerRegistration addColumnSortHandler(ColumnSortEvent.Handler handler) {
+    return handlers.addHandler(ColumnSortEvent.getType(), handler);
+  }
+
+  @Override
+  public void addColumn(IsColumn<T, ?> col, String headerString) {
+    addColumn(col, newTextHeader(headerString));
+  }
+
+  @Override
+  public void addColumn(IsColumn<T, ?> col, SafeHtml headerHtml) {
+  }
+
+  @Override
+  public void addColumn(IsColumn<T, ?> col, String headerString, String footerString) {
+    addColumn(col, newTextHeader(headerString), newTextHeader(footerString));
+  }
+
+  @Override
+  public void addColumn(IsColumn<T, ?> col, SafeHtml headerHtml, SafeHtml footerHtml) {
+    addColumn(col, newSafeHtmlHeader(headerHtml), newSafeHtmlHeader(footerHtml));
+  }
+
+  @Override
+  public void setWidth(String width, boolean isFixedLayout) {
   }
 
 }
