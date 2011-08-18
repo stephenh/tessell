f4449950a9f42e9154004b71fc66b5a42ec11a69
Stephen Haberman <stephen@exigencecorp.com>
2011-08-18 16:09:11 -0500

    Use SimplePanel for generated view classes.

--- user/src/main/java/org/gwtmpv/widgets/DelegateIsWidget.java
+++ user/src/main/java/org/gwtmpv/widgets/DelegateIsWidget.java
@@ -1,66 +1,23 @@
 package org.gwtmpv.widgets;
 
-import com.google.gwt.event.logical.shared.AttachEvent.Handler;
-import com.google.gwt.event.shared.GwtEvent;
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.SimplePanel;
 
-public class DelegateIsWidget implements IsWidget {
-
-  protected Widget widget;
-
-  protected void setWidget(final Widget widget) {
-    this.widget = widget;
-  }
-
-  @Override
-  public Widget asWidget() {
-    if (widget == null) {
-      throw new IllegalStateException("DelegateIsWidget.setWidget was not called");
-    }
-    return widget;
-  }
-
-  @Override
-  public void ensureDebugId(final String id) {
-    widget.ensureDebugId(id);
-  }
-
-  @Override
-  public int getAbsoluteLeft() {
-    return widget.getAbsoluteLeft();
-  }
-
-  @Override
-  public int getAbsoluteTop() {
-    return widget.getAbsoluteTop();
-  }
-
-  @Override
-  public int getOffsetHeight() {
-    return widget.getOffsetHeight();
-  }
-
-  @Override
-  public int getOffsetWidth() {
-    return widget.getOffsetWidth();
-  }
-
-  @Override
-  public void onBrowserEvent(final Event event) {
-    widget.onBrowserEvent(event);
-  }
-
-  @Override
-  public void fireEvent(final GwtEvent<?> event) {
-    widget.fireEvent(event);
-  }
-
-  @Override
-  public void addStyleName(final String styleName) {
-    widget.addStyleName(styleName);
-  }
+/** Base class for views generated from {@code ui.xml} files.
+ *
+ * Extending {@code SimplePanel} means we've inserted another {@code div}
+ * into the tree, but it also means we can return ourselves from {@code asWidget}.
+ *
+ * This means that, later, the client can retrieve the widget from containers
+ * (like other panels) and cast us back to the view subclasses.
+ *
+ * (Previously we returned the uibinder object itself from asWidget, but
+ * then the client had no reference back to the view.)
+ *
+ * (We also extend {@code SimplePanel} instead of {@code Composite} so
+ * that technically our uibinder widget can be stolen from us and put
+ * into another parent (like {@link RowTable} does).)
+ */
+public class DelegateIsWidget extends SimplePanel implements IsWidget {
 
   @Override
   public IsStyle getStyle() {
@@ -68,28 +25,8 @@ public class DelegateIsWidget implements IsWidget {
   }
 
   @Override
-  public String getStyleName() {
-    return widget.getStyleName();
-  }
-
-  @Override
-  public void removeStyleName(final String styleName) {
-    widget.removeStyleName(styleName);
-  }
-
-  @Override
   public IsElement getIsElement() {
-    return new GwtElement(widget.getElement());
-  }
-
-  @Override
-  public HandlerRegistration addAttachHandler(Handler handler) {
-    return widget.addAttachHandler(handler);
-  }
-
-  @Override
-  public boolean isAttached() {
-    return widget.isAttached();
+    return new GwtElement(getElement());
   }
 
 }
