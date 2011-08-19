72b25523ad5907d9fc76bd98e64cd10a2ead37c5
Stephen Haberman <stephen@exigencecorp.com>
2011-08-19 16:49:16 -0500

    Implement StubButtonBase.text/html.

--- user/src/main/java/org/gwtmpv/widgets/StubButtonBase.java
+++ user/src/main/java/org/gwtmpv/widgets/StubButtonBase.java
@@ -2,22 +2,27 @@ package org.gwtmpv.widgets;
 
 public class StubButtonBase extends StubFocusWidget implements IsButtonBase {
 
+  private String html;
+  private String text;
+
   @Override
   public String getHTML() {
-    return null;
+    return html;
   }
 
   @Override
   public void setHTML(final String html) {
+    this.html = html;
   }
 
   @Override
   public String getText() {
-    return null;
+    return text;
   }
 
   @Override
   public void setText(final String text) {
+    this.text = text;
   }
 
 }
