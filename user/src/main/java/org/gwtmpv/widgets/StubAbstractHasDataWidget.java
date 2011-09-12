30352e811534efa2486d7575e77f605676c4ecf1
Stephen Haberman <stephen@exigencecorp.com>
2011-09-12 15:19:33 -0500

    Flush out StubAbstractHasDataWidget.
    Reusing HasDataPresenter in JRE tests become untenable...for some reason.

--- user/src/main/java/org/gwtmpv/widgets/StubAbstractHasDataWidget.java
+++ user/src/main/java/org/gwtmpv/widgets/StubAbstractHasDataWidget.java
@@ -1,14 +1,9 @@
 package org.gwtmpv.widgets;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gwt.event.shared.EventHandler;
-import com.google.gwt.event.shared.GwtEvent.Type;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.cellview.client.LoadingStateChangeEvent.LoadingState;
-import com.google.gwt.user.cellview.client.PublicHasDataPresenter;
 import com.google.gwt.view.client.CellPreviewEvent;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.Range;
@@ -16,17 +11,20 @@ import com.google.gwt.view.client.RangeChangeEvent;
 import com.google.gwt.view.client.RowCountChangeEvent;
 import com.google.gwt.view.client.SelectionModel;
 
+/** GWT's {@link HasDataPresenter} is DOM coupled, so this is a mediocre attempt at reproducing it's behavior. */
 public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstractHasData<T> {
 
-  private final PublicHasDataPresenter<T> p;
+  private final List<T> rowData = new ArrayList<T>();
   private int redraws = 0;
+  private int rowCount = 0;
+  private boolean rowCountExact = false;
+  private Range visibleRange;
 
   public StubAbstractHasDataWidget() {
     this(15);
   }
 
   public StubAbstractHasDataWidget(int pageSize) {
-    p = new PublicHasDataPresenter<T>(this, new FakeView(), pageSize, null);
     setPageSize(pageSize);
   }
 
@@ -40,14 +38,12 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
 
   @Override
   public void setRowData(List<? extends T> values) {
-    // setRowCount(values.size());
-    // setVisibleRange(0, values.size());
     setRowData(0, values);
   }
 
   @Override
   public SelectionModel<? super T> getSelectionModel() {
-    return p.getSelectionModel();
+    return null;
   }
 
   @Override
@@ -57,74 +53,102 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
 
   @Override
   public int getVisibleItemCount() {
-    return p.getVisibleItemCount();
+    return Math.min(visibleRange.getLength(), rowData.size() - visibleRange.getStart());
   }
 
   @Override
   public List<T> getVisibleItems() {
-    return p.getVisibleItems();
+    return rowData.subList(visibleRange.getStart(), Math.min(rowData.size(), visibleRange.getStart() + visibleRange.getLength()));
   }
 
   @Override
   public void setRowData(final int start, final List<? extends T> values) {
-    StubScheduler.get().runWithDeferred(new Runnable() {
-      public void run() {
-        p.setRowData(start, values);
+    // copy/paste from HasDataPresenter
+    int valuesLength = values.size();
+    int valuesEnd = start + valuesLength;
+
+    // Calculate the bounded start (inclusive) and end index (exclusive).
+    int pageStart = getPageStart();
+    int pageEnd = getPageStart() + getPageSize();
+    int boundedStart = Math.max(start, pageStart);
+    int boundedEnd = Math.min(valuesEnd, pageEnd);
+    if (start != pageStart && boundedStart >= boundedEnd) {
+      // The data is out of range for the current page.
+      // Intentionally allow empty lists that start on the page start.
+      return;
+    }
+
+    // Create placeholders up to the specified index.
+    // PendingState<T> pending = ensurePendingState();
+    // int cacheOffset = Math.max(0, boundedStart - pageStart - getVisibleItemCount());
+    // for (int i = 0; i < cacheOffset; i++) {
+    //  pending.rowData.add(null);
+    //}
+
+    // Insert the new values into the data array.
+    for (int i = boundedStart; i < boundedEnd; i++) {
+      T value = values.get(i - start);
+      int dataIndex = i - pageStart;
+      if (dataIndex < getVisibleItemCount()) {
+        rowData.set(dataIndex, value);
+      } else {
+        rowData.add(value);
       }
-    });
+    }
+
+    // Remember the range that has been replaced.
+    // pending.replaceRange(boundedStart - cacheOffset, boundedEnd);
+
+    // Fire a row count change event after updating the data.
+    if (valuesEnd > getRowCount()) {
+      setRowCount(valuesEnd, isRowCountExact());
+    }
   }
 
   @Override
   public void setSelectionModel(SelectionModel<? super T> selectionModel) {
-    p.setSelectionModel(selectionModel);
   }
 
   @Override
   public void setVisibleRangeAndClearData(Range range, boolean forceRangeChangeEvent) {
-    p.setVisibleRangeAndClearData(range, forceRangeChangeEvent);
   }
 
   @Override
   public HandlerRegistration addRangeChangeHandler(RangeChangeEvent.Handler handler) {
-    return p.addRangeChangeHandler(handler);
+    return handlers.addHandler(RangeChangeEvent.getType(), handler);
   }
 
   @Override
   public HandlerRegistration addRowCountChangeHandler(RowCountChangeEvent.Handler handler) {
-    return p.addRowCountChangeHandler(handler);
+    return handlers.addHandler(RowCountChangeEvent.getType(), handler);
   }
 
   @Override
   public int getRowCount() {
-    return p.getRowCount();
+    return rowCount;
   }
 
   @Override
   public Range getVisibleRange() {
-    return p.getVisibleRange();
+    return visibleRange;
   }
 
   @Override
   public boolean isRowCountExact() {
-    return p.isRowCountExact();
+    return rowCountExact;
   }
 
   @Override
   public void setRowCount(final int count) {
-    StubScheduler.get().runWithDeferred(new Runnable() {
-      public void run() {
-        p.setRowCount(count);
-      }
-    });
+    setRowCount(count, true);
   }
 
   @Override
   public void setRowCount(final int count, final boolean isExact) {
-    StubScheduler.get().runWithDeferred(new Runnable() {
-      public void run() {
-        p.setRowCount(count, isExact);
-      }
-    });
+    rowCount = count;
+    rowCountExact = isExact;
+    // Update the pager.
+    RowCountChangeEvent.fire(this, count, isExact);
   }
 
   @Override
@@ -134,17 +158,17 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
 
   @Override
   public void setVisibleRange(Range range) {
-    p.setVisibleRange(range);
+    this.visibleRange = range;
   }
 
   @Override
   public HandlerRegistration addCellPreviewHandler(CellPreviewEvent.Handler<T> handler) {
-    return p.addCellPreviewHandler(handler);
+    return null;
   }
 
   @Override
   public ProvidesKey<T> getKeyProvider() {
-    return p.getKeyProvider();
+    return null;
   }
 
   @Override
@@ -166,22 +190,20 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
 
   @Override
   public KeyboardPagingPolicy getKeyboardPagingPolicy() {
-    return p.getKeyboardPagingPolicy();
+    return null;
   }
 
   @Override
   public void setKeyboardPagingPolicy(KeyboardPagingPolicy policy) {
-    p.setKeyboardPagingPolicy(policy);
   }
 
   @Override
   public KeyboardSelectionPolicy getKeyboardSelectionPolicy() {
-    return p.getKeyboardSelectionPolicy();
+    return null;
   }
 
   @Override
   public void setKeyboardSelectionPolicy(KeyboardSelectionPolicy policy) {
-    p.setKeyboardSelectionPolicy(policy);
   }
 
   @Override
@@ -191,7 +213,7 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
 
   @Override
   public int getPageStart() {
-    return getVisibleRange().getStart();
+    return 0;
   }
 
   @Override
@@ -207,36 +229,6 @@ public class StubAbstractHasDataWidget<T> extends StubWidget implements IsAbstra
   @Override
   public void redraw() {
     redraws++;
-  }
-
-  private final class FakeView extends PublicHasDataPresenter.PublicView<T> {
-    public <H extends EventHandler> HandlerRegistration addHandler(H handler, Type<H> type) {
-      return null;
-    }
-
-    @Override
-    public void render(SafeHtmlBuilder sb, List<T> values, int start, SelectionModel<? super T> selectionModel) {
-    }
-
-    @Override
-    public void replaceAllChildren(List<T> values, SafeHtml html, boolean stealFocus, boolean other) {
-    }
-
-    @Override
-    public void replaceChildren(List<T> values, int start, SafeHtml html, boolean stealFocus) {
-    }
-
-    @Override
-    public void resetFocus() {
-    }
-
-    @Override
-    public void setKeyboardSelected(int index, boolean selected, boolean stealFocus) {
-    }
-
-    @Override
-    public void setLoadingState(LoadingState state) {
-    }
   }
 
 }
