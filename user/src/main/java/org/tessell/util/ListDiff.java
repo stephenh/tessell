de4f6e2cd0251a1d47494ebfc858114299d335c3
Stephen Haberman <stephen@exigencecorp.com>
2012-08-09 11:08:03 -0500

    Fix NPEs in ListDiff.

--- user/src/main/java/org/tessell/util/ListDiff.java
+++ user/src/main/java/org/tessell/util/ListDiff.java
@@ -9,20 +9,22 @@ public class ListDiff<T> {
   public static <T> ListDiff<T> of(Collection<T> oldValue, Collection<T> newValue) {
     List<T> added = new ArrayList<T>();
     List<T> removed = new ArrayList<T>();
-    if (oldValue == null) {
+    if (oldValue == null && newValue != null) {
       added.addAll(newValue);
-      return new ListDiff<T>(added, removed);
-    }
-    List<T> newCopy = new ArrayList<T>(newValue);
-    for (T t : oldValue) {
-      if (!newCopy.remove(t)) {
-        removed.add(t);
+    } else if (oldValue != null && newValue == null) {
+      removed.addAll(oldValue);
+    } else if (oldValue != null && newValue != null) {
+      List<T> newCopy = new ArrayList<T>(newValue);
+      for (T t : oldValue) {
+        if (!newCopy.remove(t)) {
+          removed.add(t);
+        }
       }
-    }
-    List<T> oldCopy = new ArrayList<T>(oldValue);
-    for (T t : newValue) {
-      if (!oldCopy.remove(t)) {
-        added.add(t);
+      List<T> oldCopy = new ArrayList<T>(oldValue);
+      for (T t : newValue) {
+        if (!oldCopy.remove(t)) {
+          added.add(t);
+        }
       }
     }
     return new ListDiff<T>(added, removed);
@@ -31,7 +33,7 @@ public class ListDiff<T> {
   public final Collection<T> added;
   public final Collection<T> removed;
 
-  private ListDiff(List<T> added, List<T> removed) {
+  private ListDiff(Collection<T> added, Collection<T> removed) {
     this.added = added;
     this.removed = removed;
   }
--- user/src/test/java/org/tessell/util/ListDiffTest.java
+++ user/src/test/java/org/tessell/util/ListDiffTest.java
@@ -9,6 +9,27 @@ import org.junit.Test;
 public class ListDiffTest {
 
   @Test
+  public void nullOldValue() {
+    ListDiff<String> d = ListDiff.of(null, list("3", "3", "3"));
+    assertThat(d.added.toString(), is("[3, 3, 3]"));
+    assertThat(d.removed.size(), is(0));
+  }
+
+  @Test
+  public void nullNewValue() {
+    ListDiff<String> d = ListDiff.of(list("3", "3", "3"), null);
+    assertThat(d.removed.toString(), is("[3, 3, 3]"));
+    assertThat(d.added.size(), is(0));
+  }
+
+  @Test
+  public void nullNewAndOldValue() {
+    ListDiff<String> d = ListDiff.of(null, null);
+    assertThat(d.added.size(), is(0));
+    assertThat(d.removed.size(), is(0));
+  }
+
+  @Test
   public void testFindAdds() {
     ListDiff<String> d = ListDiff.of(list("3"), list("3", "3", "3"));
     assertThat(d.added.toString(), is("[3, 3]"));
