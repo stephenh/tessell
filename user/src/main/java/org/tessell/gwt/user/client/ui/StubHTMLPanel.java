d2a4192106dbb8429187d42be0a4aa53ba2013b7
Stephen Haberman <stephen@exigencecorp.com>
2014-01-01 15:58:26 -0600

    Add StubHTMLPanel.getAddedTo(IsElement).

--- user/src/main/java/org/tessell/gwt/user/client/ui/StubHTMLPanel.java
+++ user/src/main/java/org/tessell/gwt/user/client/ui/StubHTMLPanel.java
@@ -1,6 +1,8 @@
 package org.tessell.gwt.user.client.ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.tessell.gwt.dom.client.IsElement;
@@ -13,6 +15,7 @@ public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
   private final String html;
   private final Map<String, IsWidget> replacedById = new HashMap<String, IsWidget>();
   private final Map<IsElement, IsWidget> replacedByElement = new HashMap<IsElement, IsWidget>();
+  private final Map<IsElement, List<IsWidget>> addedByElement = new HashMap<IsElement, List<IsWidget>>();
 
   public StubHTMLPanel() {
     this("div", null);
@@ -30,6 +33,12 @@ public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
   @Override
   public void add(IsWidget widget, IsElement elem) {
     super.add(widget);
+    List<IsWidget> added = addedByElement.get(elem);
+    if (added == null) {
+      added = new ArrayList<IsWidget>();
+      addedByElement.put(elem, added);
+    }
+    added.add(widget);
   }
 
   public String getTag() {
@@ -72,6 +81,10 @@ public class StubHTMLPanel extends StubComplexPanel implements IsHTMLPanel {
       throw new IllegalArgumentException("Could not find widget for " + elem);
     }
     return w;
+  }
+
+  public List<IsWidget> getAddedTo(IsElement elem) {
+    return addedByElement.get(elem);
   }
 
   @Override
