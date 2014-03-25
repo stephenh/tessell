80d74af951de3bebee4431698efea2f31f66331c
Stephen Haberman <stephen@bizo.com>
2014-03-25 17:16:31 -0500

    Add ListDiff.moves in anticipation of smarter view binding on reorders.

--- user/src/main/java/org/tessell/util/ListDiff.java
+++ user/src/main/java/org/tessell/util/ListDiff.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Compares new/old versions of lists, e.g. for firing add/remove events.
+ */
 public class ListDiff<T> {
 
-  public static <T> ListDiff<T> of(Collection<T> oldValue, Collection<T> newValue) {
+  /** @returns a diff of {@code oldValue} and {@code newValue}. */
+  public static <T> ListDiff<T> of(List<T> oldValue, List<T> newValue) {
     List<T> added = new ArrayList<T>();
     List<T> removed = new ArrayList<T>();
+    List<NewLocation<T>> moves = new ArrayList<NewLocation<T>>();
     if (oldValue == null && newValue != null) {
       added.addAll(newValue);
     } else if (oldValue != null && newValue == null) {
@@ -24,22 +29,46 @@ public class ListDiff<T> {
       for (T t : newValue) {
         if (!oldCopy.remove(t)) {
           added.add(t);
+        } else {
+          int oldIndex = oldValue.indexOf(t);
+          int newIndex = newValue.indexOf(t);
+          if (oldIndex != newIndex) {
+            moves.add(new NewLocation<T>(t, newIndex));
+          }
         }
       }
     }
-    return new ListDiff<T>(added, removed);
+    return new ListDiff<T>(added, removed, moves);
   }
 
   public final Collection<T> added;
   public final Collection<T> removed;
+  public final Collection<NewLocation<T>> moves;
 
-  private ListDiff(Collection<T> added, Collection<T> removed) {
+  /** Tracks an element that was not added or removed, but moved to a new index in the list. */
+  public static class NewLocation<T> {
+    public final T element;
+    public final int index;
+
+    private NewLocation(T element, int index) {
+      this.element = element;
+      this.index = index;
+    }
+
+    @Override
+    public String toString() {
+      return element + "@" + index;
+    }
+  }
+
+  private ListDiff(Collection<T> added, Collection<T> removed, Collection<NewLocation<T>> moves) {
     this.added = added;
     this.removed = removed;
+    this.moves = moves;
   }
 
   @Override
   public String toString() {
-    return added + "; " + removed;
+    return added + "; " + removed + "; " + moves;
   }
 }
--- user/src/test/java/org/tessell/util/ListDiffTest.java
+++ user/src/test/java/org/tessell/util/ListDiffTest.java
@@ -13,6 +13,7 @@ public class ListDiffTest {
     ListDiff<String> d = ListDiff.of(null, list("3", "3", "3"));
     assertThat(d.added.toString(), is("[3, 3, 3]"));
     assertThat(d.removed.size(), is(0));
+    assertThat(d.moves.size(), is(0));
   }
 
   @Test
@@ -20,6 +21,7 @@ public class ListDiffTest {
     ListDiff<String> d = ListDiff.of(list("3", "3", "3"), null);
     assertThat(d.removed.toString(), is("[3, 3, 3]"));
     assertThat(d.added.size(), is(0));
+    assertThat(d.moves.size(), is(0));
   }
 
   @Test
@@ -27,6 +29,7 @@ public class ListDiffTest {
     ListDiff<String> d = ListDiff.of(null, null);
     assertThat(d.added.size(), is(0));
     assertThat(d.removed.size(), is(0));
+    assertThat(d.moves.size(), is(0));
   }
 
   @Test
@@ -34,6 +37,7 @@ public class ListDiffTest {
     ListDiff<String> d = ListDiff.of(list("3"), list("3", "3", "3"));
     assertThat(d.added.toString(), is("[3, 3]"));
     assertThat(d.removed.size(), is(0));
+    assertThat(d.moves.size(), is(0));
   }
 
   @Test
@@ -41,6 +45,42 @@ public class ListDiffTest {
     ListDiff<String> d = ListDiff.of(list("3", "3", "3"), list("3"));
     assertThat(d.removed.toString(), is("[3, 3]"));
     assertThat(d.added.size(), is(0));
+    assertThat(d.moves.size(), is(0));
+  }
+
+  @Test
+  public void testFindMoves() {
+    ListDiff<String> d = ListDiff.of(list("1", "2"), list("2", "1"));
+    assertThat(d.removed.size(), is(0));
+    assertThat(d.added.size(), is(0));
+    assertThat(d.moves.toString(), is("[2@0, 1@1]"));
+  }
+
+  @Test
+  public void testFindMoveAtTheStart() {
+    ListDiff<String> d = ListDiff.of(list("1", "2", "3"), list("2", "1", "3"));
+    assertThat(d.removed.size(), is(0));
+    assertThat(d.added.size(), is(0));
+    assertThat(d.moves.toString(), is("[2@0, 1@1]"));
+  }
+
+  @Test
+  public void testFindMoveAtTheEnd() {
+    ListDiff<String> d = ListDiff.of(list("1", "2", "3"), list("1", "3", "2"));
+    assertThat(d.removed.size(), is(0));
+    assertThat(d.added.size(), is(0));
+    assertThat(d.moves.toString(), is("[3@1, 2@2]"));
+  }
+
+  @Test
+  public void testFindMovesWithMultipleMatchingValues() {
+    ListDiff<String> d = ListDiff.of(list("1", "2", "3", "3"), list("3", "3", "1", "2"));
+    assertThat(d.removed.size(), is(0));
+    assertThat(d.added.size(), is(0));
+    // the 2nd 3 here should really be "3@1", but, hey, if you're inserting these things
+    // as value objects, then it probably doesn't matter that you'll .add(0, "3"), .add(0, "3")
+    // twice.
+    assertThat(d.moves.toString(), is("[3@0, 3@0, 1@2, 2@3]"));
   }
 
 }
