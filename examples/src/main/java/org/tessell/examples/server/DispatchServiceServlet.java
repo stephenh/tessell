675874406bdffd81b3e3115625c3808c564400b3
Stephen Haberman <stephen@exigencecorp.com>
2014-10-02 14:09:11 -0500

    Convert examples project to Gradle.

--- examples/src/main/java/org/tessell/examples/server/DispatchServiceServlet.java
+++ examples/src/main/java/org/tessell/examples/server/DispatchServiceServlet.java
@@ -2,18 +2,12 @@ package org.tessell.examples.server;
 
 import org.tessell.dispatch.server.ActionDispatch;
 import org.tessell.dispatch.server.DefaultActionDispatch;
-import org.tessell.dispatch.server.SessionIdValidator;
 import org.tessell.dispatch.server.servlet.AbstractDispatchServiceServlet;
 import org.tessell.examples.server.handlers.GetEmployeeHandler;
 
 public class DispatchServiceServlet extends AbstractDispatchServiceServlet {
 
   private static final long serialVersionUID = 1L;
-
-  @Override
-  protected SessionIdValidator getSessionValidator() {
-    return null;
-  }
 
   @Override
   protected ActionDispatch getActionDispatch() {
