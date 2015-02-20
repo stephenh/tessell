33a53d83f44a81e072e88c4c1307ba2344aba4d8
Stephen Haberman <stephen@exigencecorp.com>
2015-02-20 11:51:53 -0600

    Keep cursor from being reset when using binder.onKeyUp.

--- user/src/main/java/org/tessell/model/dsl/StringPropertyBinder.java
+++ user/src/main/java/org/tessell/model/dsl/StringPropertyBinder.java
@@ -6,6 +6,7 @@ import org.tessell.gwt.user.client.ui.IsTextBoxBase;
 import org.tessell.model.events.PropertyChangedEvent;
 import org.tessell.model.events.PropertyChangedHandler;
 import org.tessell.model.properties.StringProperty;
+import org.tessell.util.ObjectUtils;
 
 import com.google.gwt.event.dom.client.HasKeyUpHandlers;
 import com.google.gwt.event.dom.client.KeyUpEvent;
@@ -30,7 +31,13 @@ public class StringPropertyBinder extends PropertyBinder<String> {
     }
     b.add(sp.addPropertyChangedHandler(new PropertyChangedHandler<String>() {
       public void onPropertyChanged(final PropertyChangedEvent<String> event) {
-        source.setValue(event.getProperty().get(), true);
+        // Explicitly check if we need to source.setValue, because if this change
+        // is due to a key up event, then source.getValue is already correct, but
+        // calling the admittedly needless .setValue will reset the cursor to the
+        // end of the input field
+        if (!ObjectUtils.eq(event.getProperty().get(), source.getValue())) {
+          source.setValue(event.getProperty().get(), true);
+        }
       }
     }));
     // set initial value
