ea972a3075355d425d05edb9b989e3d0e103957b
Stephen Haberman <stephen@exigencecorp.com>
2015-03-03 16:05:04 -0600

    Cache the asString for IntegerProperty/LongProperty.

--- user/src/main/java/org/tessell/model/properties/IntegerProperty.java
+++ user/src/main/java/org/tessell/model/properties/IntegerProperty.java
@@ -4,6 +4,8 @@ import org.tessell.model.values.Value;
 
 public class IntegerProperty extends AbstractProperty<Integer, IntegerProperty> {
 
+  private Property<String> asString = null;
+
   public IntegerProperty(final Value<Integer> value) {
     super(value);
   }
@@ -19,15 +21,18 @@ public class IntegerProperty extends AbstractProperty<Integer, IntegerProperty> 
   }
 
   public Property<String> asString(String invalidMessage) {
-    return formatted(invalidMessage, new PropertyFormatter<Integer, String>() {
-      public String format(Integer a) {
-        return Integer.toString(a);
-      }
+    if (asString == null) {
+      asString = formatted(invalidMessage, new PropertyFormatter<Integer, String>() {
+        public String format(Integer a) {
+          return Integer.toString(a);
+        }
 
-      @Override
-      public Integer parse(String b) {
-        return Integer.parseInt(b);
-      }
-    });
+        @Override
+        public Integer parse(String b) {
+          return Integer.parseInt(b);
+        }
+      });
+    }
+    return asString;
   }
 }
--- user/src/main/java/org/tessell/model/properties/LongProperty.java
+++ user/src/main/java/org/tessell/model/properties/LongProperty.java
@@ -4,6 +4,8 @@ import org.tessell.model.values.Value;
 
 public class LongProperty extends AbstractProperty<Long, LongProperty> {
 
+  private Property<String> asString = null;
+
   public LongProperty(final Value<Long> value) {
     super(value);
   }
@@ -14,15 +16,18 @@ public class LongProperty extends AbstractProperty<Long, LongProperty> {
   }
 
   public Property<String> asString() {
-    return formatted(new PropertyFormatter<Long, String>() {
-      public String format(Long a) {
-        return Long.toString(a);
-      }
+    if (asString == null) {
+      asString = formatted(new PropertyFormatter<Long, String>() {
+        public String format(Long a) {
+          return Long.toString(a);
+        }
 
-      @Override
-      public Long parse(String b) {
-        return Long.parseLong(b);
-      }
-    });
+        @Override
+        public Long parse(String b) {
+          return Long.parseLong(b);
+        }
+      });
+    }
+    return asString;
   }
 }
