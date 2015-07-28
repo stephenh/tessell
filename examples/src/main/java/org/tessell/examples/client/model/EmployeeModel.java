2c2d5a41f77179d39812ca9cca81b1984d62e007
Stephen Haberman <stephen.haberman@gmail.com>
2015-07-28 07:42:59 -0500

    Restore examples with fixed dtonator.

--- examples/src/main/java/org/tessell/examples/client/model/EmployeeModel.java
+++ examples/src/main/java/org/tessell/examples/client/model/EmployeeModel.java
@@ -5,6 +5,7 @@ import org.tessell.examples.shared.dtos.EmployeeDto;
 public class EmployeeModel extends EmployeeModelCodegen {
 
   public EmployeeModel(EmployeeDto dto) {
+    super(dto);
     addRules();
     merge(dto);
   }
