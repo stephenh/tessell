f6172134fd66ac179d51aadab4a62fd0a18a09e7
Stephen Haberman <stephen.haberman@gmail.com>
2015-11-11 10:49:42 -0600

    Add FunctionalInterface to Condition.

--- user/src/main/java/org/tessell/model/properties/Condition.java
+++ user/src/main/java/org/tessell/model/properties/Condition.java
@@ -1,6 +1,7 @@
 package org.tessell.model.properties;
 
 /** An interface for evaluating the value of properties. */
+@FunctionalInterface
 public interface Condition<P> {
 
   /** @return true if {@code value} passes the implementation-defined criteria. */
